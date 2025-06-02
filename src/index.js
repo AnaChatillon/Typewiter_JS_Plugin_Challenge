bdd2e55b8886ef2b20f589ebd53ddf94c11ea970
Ana Chatillon <a_chantil@hotmail.com>
2025-06-02 21:58:37 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//Challenge 1 - paste typewriter.js on index.html as <script>
+
+//Challenge 2
+let challenge2Element = document.querySelector("#challenge-2");
+challenge2Element.innerHTML = "Ana Chatillon";
+
+//Challenge 3
+new Typewriter("#challenge-3", {
+  strings: "Ana Chatillon",
+  autoStart: true,
+  cursor: "",
+  delay: 150,
+});
